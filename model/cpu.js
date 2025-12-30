d1d49bae3d1f6a1e627c8c46fb0b2a7916d801f3
JiuYann <sakura_net@163.com>
2025-12-30 21:03:50 +0800

    优化cpu负载计算格式，确保小数点后保留两位

--- model/cpu.js
+++ model/cpu.js
@@ -12,7 +12,7 @@ export function getCpuInfo() {
             name: cpuInfo[0].model.trim().slice(0,21).concat('...'), //cpu型号
             arch: os.arch(), //cpu架构
             core: cpuInfo.length, //核心数
-            load: `${os.loadavg()[0]/cpuInfo.length*100}%`,
+            load: `${(os.loadavg()[0]/cpuInfo.length*100).toFixed(2)}%`,
             speed: (cpuInfo[0].speed / 1000).toFixed(2), //频率
         }
     } catch(error){
